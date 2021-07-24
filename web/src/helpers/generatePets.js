843b47d534f307a152ddd6da6eee32772bf6f761
bulentzeki <bzekiunat@gmail.com>
2021-07-24 12:46:16 +0300

    Update pet ids number to string
    pet id should be unique and string

--- web/src/helpers/generatePets.js
+++ web/src/helpers/generatePets.js
@@ -18,7 +18,7 @@ const generatePets = (numberOfPets = 3) => {
   const petList = [];
   for(let i = 0; i < numberOfPets; i += 1) {
     petList.push({
-      id: i,
+      id: i.toString(),
       name: PET_NAMES[Math.floor(Math.random()*PET_NAMES.length)],
       type: PET_TYPES[Math.floor(Math.random()*PET_TYPES.length)],
       age: Math.floor(Math.random() * 15),
