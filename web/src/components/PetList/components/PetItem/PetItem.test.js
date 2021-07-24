5143ce43a9bc334d4c7252ea97bb688619400191
bulentzeki <bzekiunat@gmail.com>
2021-07-24 22:39:10 +0300

    Fixes PetItem test by wrapping the component with Provider

--- web/src/components/PetList/components/PetItem/PetItem.test.js
+++ web/src/components/PetList/components/PetItem/PetItem.test.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { generatePets } from '../../../../helpers/generatePets';
+import { Provider } from 'react-redux';
+import { store } from '../../../../app/store';
 import PetItem from './PetItem';
-
 
 const pets = generatePets();
 
 test('renders pet image', () => {
     const { getByRole } = render(
-        <PetItem pet={pets[0]} />
+        <Provider store={store}>
+            <PetItem pet={pets[0]} />
+        </Provider>
     );
     expect(getByRole('img', { name: 'pet' })).toBeInTheDocument()
 });
 
 test('renders pet name', () => {
     const { getByText } = render(
-        <PetItem pet={pets[0]} />
+        <Provider store={store}>
+            <PetItem pet={pets[0]} />
+        </Provider>
     );
     expect(getByText(/Name/)).toBeInTheDocument();
     expect(getByText(pets[0].name)).toBeInTheDocument();
@@ -23,7 +28,9 @@ test('renders pet name', () => {
 
 test('renders pet animal type', () => {
     const { getByText } = render(
-        <PetItem pet={pets[0]} />
+        <Provider store={store}>
+            <PetItem pet={pets[0]} />
+        </Provider>
     );
     expect(getByText(/Animal Type/)).toBeInTheDocument();
     expect(getByText(pets[0].type)).toBeInTheDocument();
@@ -31,7 +38,9 @@ test('renders pet animal type', () => {
 
 test('renders number of feeds', () => {
     const { getByText } = render(
-        <PetItem pet={pets[0]} />
+        <Provider store={store}>
+            <PetItem pet={pets[0]} />
+        </Provider>
     );
     expect(getByText(/Number of feeds/)).toBeInTheDocument();
     expect(getByText(`${pets[0].feeds}`)).toBeInTheDocument();
