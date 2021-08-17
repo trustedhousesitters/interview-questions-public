1b3553a061631963afa3fa0bdefd6476801c6a69
eTheMan80 <cendibe@yahoo.co.uk>
2021-08-17 10:53:34 +0100

    Updated app to add and remove items

--- web/src/components/PetList/PetList.js
+++ web/src/components/PetList/PetList.js
@@ -1,16 +1,61 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 import PetItem from './components/PetItem';
 import { getPets } from './selectors';
 import './PetList.css';
 
+const StyledItems = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0 0 20px;
+`;
+
+const StyledForm = styled.form`
+  padding: 10px 5px;
+`;
+
 const PetList = () => {
     const pets = useSelector(getPets);
+    const [petsList, setPetsList] = useState(pets);
+    const [newPetItem, setPetItem] = useState('');
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://github.com/AdenFlorian/random.dog', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      } catch (error) {
+        throw new Error('Failed to fetch data');
+      }
+    };
+
+    const removeItem = id => {
+    const newPetList = petsList.filter(items => items.id !== id);
+      setPetsList(newPetList);
+    };
+    const newlyAddedItem = {id: petsList.length + 10, name: newPetItem, type: 'Animal', feeds: 2};
+    const listItems = [...petsList, newlyAddedItem];
+    const handleChange = event => setPetItem(event.target.value);
+    const addNewPetItem = e => {
+      e.preventDefault();
+      setPetsList(listItems);
+      setPetItem('');
+      // fetchData(); I couldn't fetch the data due to a CORS issue.
+    };
     return (
         <Fragment>
             <h1 className="Pets-title">My Pets</h1>
-            { pets.length > 1 && pets.map(pet => <PetItem pet={pet} key={pet.id}/>) }
+            <StyledForm>
+              <input type="text" id="name" name="name" value={newPetItem} onChange={handleChange} />
+              <button onClick={addNewPetItem}>Submit</button>
+            </StyledForm>
+            <StyledItems>
+            { pets.length > 1 && petsList.map(pet => <PetItem pet={pet} key={pet.id} removeItem={removeItem} />) }
+            </StyledItems>
         </Fragment>
     );
 };
--- web/src/components/PetList/components/PetItem/PetItem.js
+++ web/src/components/PetList/components/PetItem/PetItem.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import './PetItem.css';
 import close from './assets/close.svg';
 import dog from './assets/PetsPlaceholder/Dog.svg'
 
-const PetItem = ({ pet }) => {
+const StyledPetItems = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: solid 1px #D5D5D9;
+  box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.06);
+  margin-bottom: 10px;
+  padding: 20px;
+  margin: 0 0 10px;
+  flex: 1 1 100%;
+
+  @media (min-width: 768px) {
+    flex: 0 1 42%;
+    margin: 0 5px 10px;
+  }
+
+  @media (min-width: 1024px) {
+    flex: 0 1 25%;
+  }
+`;
+
+const PetItem = ({ pet, removeItem }) => {
     const { name, type, feeds } = pet;
     const imageUrl = pet.imageUrl || dog;
+    const deleteItem = () => removeItem(pet.id);
     return (
-        <div className="Pet-item">
+        <StyledPetItems>
             <div>
                 <img src={imageUrl} className="Pet-image" alt="pet" />
             </div>
@@ -23,10 +46,10 @@ const PetItem = ({ pet }) => {
                     <span className="Pet-details-label">Number of feeds: </span><span>{feeds}</span>
                 </div>
             </div>
-            <button className="Delete-button">
+            <button className="Delete-button" onClick={deleteItem}>
                 <img src={close} className="Delete-icon" alt="delete" />
             </button>
-        </div>
+        </StyledPetItems>
     );
 };
 
