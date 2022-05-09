3f78b43a3a862bb50d8e693499f6b72b3bfe4a6b
Fernando López <fnandolopezsanchez@gmail.com>
2022-05-09 11:32:38 +0100

    Catch error for fetching dog image

--- web/src/api/fetchDogPicture.js
+++ web/src/api/fetchDogPicture.js
@@ -1,5 +1,10 @@
 export const fetchDogPicture = async ()=> {
-    const result = await fetch(`https://random.dog/woof?include=jpg`)
-    const id = await result.text()
-    return id
+    try {
+        const result = await fetch(`https://random.dog/woof?include=jpg`)
+        const id = await result.text()
+        return id
+
+    } catch(error) {
+        return error
+    }
 };
