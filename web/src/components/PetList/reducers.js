fd53e307648ed6e824cbc439d030cebc266a87b9
Scott Cox <scottcox89@gmail.com>
2022-05-17 15:31:29 +0100

    Create pets slice

--- web/src/components/PetList/reducers.js
+++ web/src/components/PetList/reducers.js
@@ -1,12 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { generatePets } from '../../helpers/generatePets';
 
 const initialState = {
   pets: generatePets(13),
 };
 
-export default function appReducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
+const slice = createSlice({
+  name: 'pets',
+  initialState,
+  reducers: {
+    deletePetById: (state, { payload }) => {
+      if (payload !== undefined) {
+        state.pets = state.pets.filter(({ id }) => id !== payload);
+      }
+    },
+  },
+});
+
+export const { deletePetById } = slice.actions;
+
+export default slice.reducer;
