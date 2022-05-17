970ebb943a5d9ba20d0e37fd8527391fd43e7ca2
Scott Cox <scottcox89@gmail.com>
2022-05-17 15:31:52 +0100

    Add deletePetById call to delete button

--- web/src/components/PetList/components/PetItem/PetItem.js
+++ web/src/components/PetList/components/PetItem/PetItem.js
@@ -1,33 +1,45 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deletePetById } from '../../reducers';
 
 import './PetItem.css';
 import close from './assets/close.svg';
-import dog from './assets/PetsPlaceholder/Dog.svg'
+import dog from './assets/PetsPlaceholder/Dog.svg';
 
 const PetItem = ({ pet }) => {
-    const { name, type, feeds } = pet;
-    const imageUrl = pet.imageUrl || dog;
-    return (
-        <div className="Pet-item">
-            <div>
-                <img src={imageUrl} className="Pet-image" alt="pet" />
-            </div>
-            <div>
-                <div>
-                    <span className="Pet-details-label">Name: </span><span>{name}</span>
-                </div>
-                <div>
-                    <span className="Pet-details-label">Animal Type: </span><span>{type}</span>
-                </div>
-                <div>
-                    <span className="Pet-details-label">Number of feeds: </span><span>{feeds}</span>
-                </div>
-            </div>
-            <button className="Delete-button">
-                <img src={close} className="Delete-icon" alt="delete" />
-            </button>
+  const { id, name, type, feeds } = pet;
+  const imageUrl = pet.imageUrl || dog;
+  const dispatch = useDispatch();
+
+  return (
+    <div className="Pet-item">
+      <div>
+        <img src={imageUrl} className="Pet-image" alt="pet" />
+      </div>
+      <div>
+        <div>
+          <span className="Pet-details-label">Name: </span>
+          <span>{name}</span>
         </div>
-    );
+        <div>
+          <span className="Pet-details-label">Animal Type: </span>
+          <span>{type}</span>
+        </div>
+        <div>
+          <span className="Pet-details-label">Number of feeds: </span>
+          <span>{feeds}</span>
+        </div>
+      </div>
+      <button
+        className="Delete-button"
+        onClick={() => {
+          dispatch(deletePetById(id));
+        }}
+      >
+        <img src={close} className="Delete-icon" alt="delete" />
+      </button>
+    </div>
+  );
 };
 
 export default PetItem;
