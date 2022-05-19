d41cf1bfc0f586e13c4366d51ce891f0973502c8
Scott Cox <scottcox89@gmail.com>
2022-05-19 20:23:42 +0100

    Fix <PetItem /> tests

--- web/src/components/PetList/components/PetItem/PetItem.test.js
+++ web/src/components/PetList/components/PetItem/PetItem.test.js
@@ -1,38 +1,37 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import { generatePets } from '../../../../helpers/generatePets';
-import PetItem from './PetItem';
+import React from 'react';
+import { Provider } from 'react-redux';
 
+import PetItem from './PetItem';
+import { store } from '../../../../app/store';
+import { generatePets } from '../../../../helpers/generatePets';
 
 const pets = generatePets();
+const WrappedPetItem = () => (
+  <Provider store={store}>
+    <PetItem pet={pets[0]} />
+  </Provider>
+);
 
 test('renders pet image', () => {
-    const { getByRole } = render(
-        <PetItem pet={pets[0]} />
-    );
-    expect(getByRole('img', { name: 'pet' })).toBeInTheDocument()
+  const { getByRole } = render(<WrappedPetItem />);
+  expect(getByRole('img', { name: 'pet' })).toBeInTheDocument();
 });
 
 test('renders pet name', () => {
-    const { getByText } = render(
-        <PetItem pet={pets[0]} />
-    );
-    expect(getByText(/Name/)).toBeInTheDocument();
-    expect(getByText(pets[0].name)).toBeInTheDocument();
+  const { getByText } = render(<WrappedPetItem />);
+  expect(getByText(/Name/)).toBeInTheDocument();
+  expect(getByText(pets[0].name)).toBeInTheDocument();
 });
 
 test('renders pet animal type', () => {
-    const { getByText } = render(
-        <PetItem pet={pets[0]} />
-    );
-    expect(getByText(/Animal Type/)).toBeInTheDocument();
-    expect(getByText(pets[0].type)).toBeInTheDocument();
+  const { getByText } = render(<WrappedPetItem />);
+  expect(getByText(/Animal Type/)).toBeInTheDocument();
+  expect(getByText(pets[0].type)).toBeInTheDocument();
 });
 
 test('renders number of feeds', () => {
-    const { getByText } = render(
-        <PetItem pet={pets[0]} />
-    );
-    expect(getByText(/Number of feeds/)).toBeInTheDocument();
-    expect(getByText(`${pets[0].feeds}`)).toBeInTheDocument();
+  const { getByText } = render(<WrappedPetItem />);
+  expect(getByText(/Number of feeds/)).toBeInTheDocument();
+  expect(getByText(`${pets[0].feeds}`)).toBeInTheDocument();
 });
