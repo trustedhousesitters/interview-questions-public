a5960995a90be206d37ca6c9abc93c219c263a62
Scott Cox <scottcox89@gmail.com>
2022-05-19 17:49:30 +0100

    Create .Pets-content div element

--- web/src/components/PetList/PetList.js
+++ web/src/components/PetList/PetList.js
@@ -3,16 +3,19 @@ import { useSelector } from 'react-redux';
 
 import PetItem from './components/PetItem';
 import { getPets } from './selectors';
+
 import './PetList.css';
 
 const PetList = () => {
-    const pets = useSelector(getPets);
-    return (
-        <Fragment>
-            <h1 className="Pets-title">My Pets</h1>
-            { pets.length > 1 && pets.map(pet => <PetItem pet={pet} key={pet.id}/>) }
-        </Fragment>
-    );
+  const pets = useSelector(getPets);
+  return (
+    <Fragment>
+      <h1 className="Pets-title">My Pets</h1>
+      <div className="Pets-content">
+        {pets.length > 1 && pets.map(pet => <PetItem pet={pet} key={pet.id} />)}
+      </div>
+    </Fragment>
+  );
 };
 
 export default PetList;
