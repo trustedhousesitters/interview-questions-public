a61bf3888daf67cb7f3054b8ea98ee7970211539
Scott Cox <scottcox89@gmail.com>
2022-05-19 18:06:32 +0100

    Add initial addPet form elements

--- web/src/components/PetList/PetList.js
+++ web/src/components/PetList/PetList.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import PetItem from './components/PetItem';
@@ -8,9 +8,27 @@ import './PetList.css';
 
 const PetList = () => {
   const pets = useSelector(getPets);
+
+  const [petName, setPetName] = useState('');
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    console.log('Add addPet dispatch here');
+  };
+
+  const handleOnChange = e => {
+    setPetName(e.target.value);
+  };
+
   return (
     <Fragment>
       <h1 className="Pets-title">My Pets</h1>
+      <div className="Pets-add-pet">
+        <form onSubmit={handleOnSubmit}>
+          <input value={petName} onChange={handleOnChange} />
+          <button type="submit">+</button>
+        </form>
+      </div>
       <div className="Pets-content">
         {pets.length > 1 && pets.map(pet => <PetItem pet={pet} key={pet.id} />)}
       </div>
