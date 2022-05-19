44ded9eeb83cc085db936ee069ed40765d4b31d9
Scott Cox <scottcox89@gmail.com>
2022-05-19 17:49:19 +0100

    Rearrange imports

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
