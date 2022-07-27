d4fbb4d0ead247a8e91da65a672e5e8ee721e6c8
Sam Gerrard <sam.gerrard@boots.co.uk>
2022-07-27 22:48:12 +0100

    Added PetForm to PetList component, with ability to show / hide the form

--- web/src/components/PetList/PetList.js
+++ web/src/components/PetList/PetList.js
@@ -1,18 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import PetItem from './components/PetItem';
 import { getPets } from './selectors';
 import './PetList.css';
+import PetForm from './components/PetForm/PetForm';
 
 const PetList = () => {
     const pets = useSelector(getPets);
+    const [isAddPetFormVisible, setAddPetFormVisible] = useState(false);
+
+    const showPetForm = () => {
+        setAddPetFormVisible(true);
+    };
+
+    const hidePetForm = () => {
+        setAddPetFormVisible(false);
+    };
 
     return (
         <Fragment>
-            <h1 className='Pets-title'>My Pets</h1>
-            {pets.length >= 1 &&
-                pets.map((pet) => <PetItem pet={pet} key={pet.id} />)}
+            <h1 className='Pet-title'>My Pets</h1>
+            {isAddPetFormVisible ? (
+                <PetForm onCancel={hidePetForm} />
+            ) : (
+                <button
+                    className='Button'
+                    onClick={showPetForm}
+                    aria-label='Add new pet'
+                >
+                    Add pet
+                </button>
+            )}
+            <div className='Pet-list'>
+                {pets.length >= 1 &&
+                    pets.map((pet) => <PetItem pet={pet} key={pet.id} />)}
+            </div>
         </Fragment>
     );
 };
