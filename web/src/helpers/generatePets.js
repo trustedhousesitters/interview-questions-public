6e42ede693190601f0fdc520acb518ae57c6abac
Kristian Roebuck <k@roeb.uk>
2022-08-02 22:09:13 +0100

    Refactor: generatePets
     * Replace for loop with map
     * Add tests

--- web/src/helpers/generatePets.js
+++ web/src/helpers/generatePets.js
@@ -1,3 +1,16 @@
+// @ts-check
+
+/**
+ * Our main pet object
+ * @typedef {Object} Pet
+ * @property {number} id - The unique ID for the Pet
+ * @property {string} name - The name of the Pet
+ * @property {string} type - Animal classification. eg Dog, Cat, etc.
+ * @property {number} feeds - How many times the pet has been fed
+ * @property {string} [imageUrl] - A url to the image of the Pet
+ */
+
+
 const PET_NAMES = [
     'Mr. Fluffings',
     'The Whiskertron',
@@ -13,20 +26,23 @@ const PET_TYPES = [
     'Wild Boar',
     'Rock',
 ];
-  
+
+
+/**
+ * Returns the specified number of randomly generated pets 
+ * @param {number} [numberOfPets=3]
+ * @returns {Pet[]}
+ */
 const generatePets = (numberOfPets = 3) => {
-  const petList = [];
-  for(let i = 0; i < numberOfPets; i += 1) {
-    petList.push({
-      id: i,
-      name: PET_NAMES[Math.floor(Math.random()*PET_NAMES.length)],
-      type: PET_TYPES[Math.floor(Math.random()*PET_TYPES.length)],
+  return Array.from({ length: numberOfPets }).map((_, i) => (
+    {
+      id: i + 1,
+      name: PET_NAMES[Math.floor(Math.random() * PET_NAMES.length)],
+      type: PET_TYPES[Math.floor(Math.random() * PET_TYPES.length)],
       age: Math.floor(Math.random() * 15),
       feeds: Math.floor(Math.random() * 6) + 1,
     })
-  }
-
-  return petList;
+  )
 };
 
 export { generatePets };
