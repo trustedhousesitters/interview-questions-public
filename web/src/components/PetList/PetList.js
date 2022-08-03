e4202a8a1eb6a8756b51349e6d686fa89f661907
Kristian Roebuck <k@roeb.uk>
2022-08-03 08:12:13 +0100

    Feature: Improve Accessibility
     * Only show delete outline with focus-visible
     * Ensure SR focus is managed when last pet is deleted

--- web/src/components/PetList/PetList.js
+++ web/src/components/PetList/PetList.js
@@ -1,17 +1,27 @@
-import React, { Fragment } from 'react';
+// @ts-check
+import React, { Fragment, useEffect, useRef } from 'react';
 
 import PetItem from './components/PetItem';
 
 import './PetList.css';
 
 const PetList = ({ dispatch, pets }) => {
+  /** @type {React.MutableRefObject<HTMLDivElement|null>} */
+  const noPetsText = useRef(null);
+
+  useEffect(() => {
+    if (pets.length === 0) {
+      noPetsText?.current?.focus();
+    }
+  }, [pets.length]);
+
   return (
     <Fragment>
       <h1 className="Pets-title">My Pets</h1>
 
       {pets.length === 0 ? (
-        <div className="Pets-no-pets" data-testid="pet-list-empty">
-          <h2>No Pets</h2>
+        <div className="Pets-no-pets" ref={noPetsText} tabIndex={-1} data-testid="pet-list-empty">
+          <h2>You don't currently have any pets.</h2>
         </div>
       ) : (
         <div className="Pets-container">
