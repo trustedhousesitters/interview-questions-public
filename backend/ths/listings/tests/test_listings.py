48696002d6ebfc830c1e21f4cb1669cb6d1934e0
Danny (on Mint19) <danny.barthaud@open.ac.uk>
2023-01-26 20:46:17 +0000

    mv listing tests into specific file

--- backend/ths/listings/tests/test_listings.py
+++ backend/ths/listings/tests/test_listings.py
@@ -1,14 +1,15 @@
-from datetime import date
-
+from datetime import date, timedelta
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Listing, Assignment
+from ..models import Listing, Assignment
 
 
 class ListingList(APITestCase):
     def setUp(self):
-        self.listing_1 = Listing.objects.create(first_name="Ross", last_name="Geller")
-        self.listing_2 = Listing.objects.create(first_name="Phoebe", last_name="Buffay")
+        self.listing_1 = Listing.objects.create(
+            first_name="Ross", last_name="Geller")
+        self.listing_2 = Listing.objects.create(
+            first_name="Phoebe", last_name="Buffay")
         self.assignment_1 = Assignment.objects.create(
             start_date=date(2023, 2, 7),
             end_date=date(2023, 2, 15),
@@ -24,7 +25,7 @@ class ListingList(APITestCase):
         response = self.client.get("/listings/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_data(self):
+    def test_get_listings(self):
         response = self.client.get("/listings/")
         self.assertEqual(
             response.data,
