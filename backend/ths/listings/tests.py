da44642b50420e1db9298f6f6b95a00adbd70878
Jamie Mayer <mail@jamiemayer.co.uk>
2023-02-11 17:41:55 +0000

    Change tests to use pytest. Also add a `test-requirements.txt` file.

--- backend/ths/listings/tests.py
+++ backend/ths/listings/tests.py
@@ -1,12 +1,26 @@
 from datetime import date
 
+import pytest
 from rest_framework import status
-from rest_framework.test import APITestCase
-from .models import Listing, Assignment
+
+from backend.ths.listings.models import Assignment, Listing
 
 
-class ListingList(APITestCase):
-    def setUp(self):
+@pytest.mark.django_db
+class TestListingList:
+    """
+    Test for ListingList API View.
+    """
+
+    listing_1: Listing
+    listing_2: Listing
+    assignment_1: Assignment
+    assignment_2: Assignment
+
+    def setup_method(self):
+        """
+        Setup boilerplate before each test case.
+        """
         self.listing_1 = Listing.objects.create(first_name="Ross", last_name="Geller")
         self.listing_2 = Listing.objects.create(first_name="Phoebe", last_name="Buffay")
         self.assignment_1 = Assignment.objects.create(
@@ -20,26 +34,31 @@ class ListingList(APITestCase):
             listing=self.listing_2,
         )
 
-    def test_get_200(self):
-        response = self.client.get("/listings/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_get_200(self, client):
+        """
+        A response code of 200 is given for a valid call to the endpoint.
+        """
+        response = client.get("/listings/")
+        assert response.status_code == status.HTTP_200_OK
 
-    def test_get_data(self):
-        response = self.client.get("/listings/")
-        self.assertEqual(
-            response.data,
-            [
-                {
-                    "first_name": self.listing_1.first_name,
-                    "last_name": self.listing_1.last_name,
-                    "pets": [],
-                    "assignments": [self.assignment_1.pk],
-                },
-                {
-                    "first_name": self.listing_2.first_name,
-                    "last_name": self.listing_2.last_name,
-                    "pets": [],
-                    "assignments": [self.assignment_2.pk],
-                },
-            ],
-        )
+    def test_get_data(self, client):
+        """
+        Returned data is as expected when calling the `listings` endpoint.
+        """
+        expected_response = [
+            {
+                "first_name": self.listing_1.first_name,
+                "last_name": self.listing_1.last_name,
+                "pets": [],
+                "assignments": [self.assignment_1.pk],
+            },
+            {
+                "first_name": self.listing_2.first_name,
+                "last_name": self.listing_2.last_name,
+                "pets": [],
+                "assignments": [self.assignment_2.pk],
+            },
+        ]
+
+        response = client.get("/listings/")
+        assert response.data == expected_response
