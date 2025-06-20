b9c4317e04270a75c7ccc896b9a0ff380416e8eb
Sam Gerrard <sam.gerrard@trustedhousesitters.com>
2025-06-20 13:17:57 +0100

    Converted rendering to react 18, cleaned up html

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
