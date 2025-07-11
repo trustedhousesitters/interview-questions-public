9b6150ef41a89dd3c204a321af82221c67768d79
Daniel Elvey <delvey21@hotmail.co.uk>
2025-07-11 13:33:07 +0100

    mock pet data is fetched and displayed using provided PetItem component

--- web/src/components/PetList/PetList.jsx
+++ web/src/components/PetList/PetList.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PetList.css";
+import PetItem from "./components/PetItem/PetItem";
 
 const PetList = () => {
+  const [petData, setPetData] = useState([]);
+  console.log(petData);
+
+  useEffect(() => {
+    async function getPetData() {
+      const url = "/api/pets";
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        setPetData(json);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    getPetData();
+  }, []);
+
   return (
     <>
       <h1 className="Pets-title">My Pets</h1>
+      {petData.length > 0
+        ? petData.map((pet) => {
+            return <PetItem pet={pet} key={pet.id} />;
+          })
+        : ""}
     </>
   );
 };
