d5fe315cd577dda7f37c3eee57fd06898fc809f8
Nosa Obaseki <fredobaseki@gmail.com>
2025-10-06 12:00:36 +0100

    fetch pets data from mock API

--- web/src/components/PetList/PetList.jsx
+++ web/src/components/PetList/PetList.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./PetList.css";
 
 const PetList = () => {
   const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      const response = await fetch("/api/pets");
+      const data = await response.json();
+      setPets(data);
+    };
+
+    fetchPets();
+  }, []);
 
   return (
     <>
