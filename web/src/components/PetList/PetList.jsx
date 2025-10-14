03d206608b9cb3fed6b84e70b635c11ced44b6d2
audreyTrustedH <audrey+th@devs.page>
2025-10-14 16:47:46 +0100

    query mock api + display results

--- web/src/components/PetList/PetList.jsx
+++ web/src/components/PetList/PetList.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import PetItem from "./components/PetItem";
 import "./PetList.css";
 
 const PetList = () => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/pets")
+      .then((response) => response.json())
+      .then((data) => setPets(data));
+  }, []);
+
   return (
     <>
       <h1 className="Pets-title">My Pets</h1>
+      {pets.map((pet) => (
+        <PetItem key={pet.id} pet={pet} />
+      ))}
     </>
   );
 };
